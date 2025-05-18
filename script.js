26b4b0777982594157b8a62482962dee35c5239d
Zerker <84744670+Zerker-TF@users.noreply.github.com>
2025-05-18 17:06:49 -0300

    added random rgb color function

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ const restartButton = document.createElement("button");
 buttonContainer.textContent = "Reset game";
 restartButton.id = "restartButton";
 
+//funcion para elegir un color RGB aleatorio
+function randomRGB( ){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+
 //funcion para crear el grid 16x16
 
 function createGrid(row,col){
@@ -22,6 +30,20 @@ function createGrid(row,col){
             gridContainer.appendChild(gridDiv);
             //que se divida por la cantidad de filas
             gridDiv.style.flex = `0 0 calc(100% / ${row})`;
+            gridDiv.hoverCount = 0;
+            //llamamos una funcion para que eliga cualquier color
+            gridDiv.style.backgroundColor = randomRGB();
+            //Si paso mas de una vez por el mismo cuadro, se oscurece un 10%
+            if(gridDiv.hoverCount < 10) {
+                gridDiv.hoverCount++;
+                gridDiv.style.opacity = gridDiv.hoverCount * 0.1;
+            }
         }
     }
 }
+
+createGrid(16,16);
+
+restartButton.addEventListener("click", () => {
+    createGrid(16,16);
+})
